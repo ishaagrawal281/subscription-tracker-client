ec66a8aac83e85451114a3203e8902ef5a47d361
Isha <ishaagrawal281@gmail.com>
2026-05-28 17:16:34 +0530

    fix axios baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://subscription-tracker-server-4j9m.onrender.com",
+  baseURL: "https://subscription-tracker-server-4j9m.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
